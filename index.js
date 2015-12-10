1166326bfc6f86005da52f520f981509ad16ecc0
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-12-10 08:57:21 +0200

    1.0.2

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 
-var typeOf = module.exports.typeOf = function(obj, objName){
+var of = module.exports.typeOf = function(obj, objName){
 	if(objName==undefined){
 		return {}.toString.call(obj).toLowerCase().match(/^[\[]object[\s]+([a-zA-Z]+)[\]]$/)[1];
 	}
-	else if(typeOf(objName)=='string'){
-		return typeOf(obj)==objName;
+	else if(of(objName)=='string'){
+		return of(obj)==objName;
 	}
-	else if(typeOf(objName)=='array'){
+	else if(of(objName)=='array'){
 		return objName.some(function(objNameOne){
-			return typeOf(obj, objNameOne);
+			return of(obj, objNameOne);
 		});
 	}
 	else{
-		throw 'Error in typeOf: objName unknown';
+		throw 'Error in type.of: objName unknown';
 	}
+};
+
+var ofs = module.exports.typesOf = function(objs, objName){
+	if(of(objs, 'array')){
+		return objs.some(function(objOne){
+			return !of(objOne, objName);
+		});
+	}
+	else return of(objs, objName);
 };
