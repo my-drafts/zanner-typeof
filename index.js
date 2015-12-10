34e19f579769012b0efc33fa2bc72af61dd08f8e
Sergej Kukharev <sergej.kucharev@gmail.com>
2015-12-10 09:32:55 +0200

    1.0.3

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 
-var of = module.exports.typeOf = function(obj, objName){
-	if(objName==undefined){
-		return {}.toString.call(obj).toLowerCase().match(/^[\[]object[\s]+([a-zA-Z]+)[\]]$/)[1];
-	}
-	else if(of(objName)=='string'){
-		return of(obj)==objName;
-	}
-	else if(of(objName)=='array'){
-		return objName.some(function(objNameOne){
-			return of(obj, objNameOne);
-		});
-	}
-	else{
-		throw 'Error in type.of: objName unknown';
+var is = module.exports.is = function(obj){
+	return {}.toString.call(obj).toLowerCase().match(/^[\[]object[\s]+([a-zA-Z]+)[\]]$/)[1];
+};
+
+var of = module.exports.of = function(obj, objName){
+	switch(is(objName)){
+		case 'array':
+			return objName.some(function(objNameOne){
+				return of(obj, objNameOne);
+			});
+		case 'string':
+			return is(obj)==objName;
+		case 'undefined':
+			return is(obj);
+		default:
+			throw 'Error in type.of: objName unknown';
 	}
 };
 
-var ofs = module.exports.typesOf = function(objs, objName){
-	if(of(objs, 'array')){
-		return objs.some(function(objOne){
-			return !of(objOne, objName);
-		});
+var ofs = module.exports.ofs = function(objs, objName){
+	switch(of(objs)){
+		case 'array':
+			return !objs.some(function(objOne){
+				return !of(objOne, objName);
+			});
+		default:
+			throw 'Error in type.ofs: objs unknown';
 	}
-	else return of(objs, objName);
 };
--- test/index.js
+++ test/index.js
@@ -2,15 +2,15 @@
 var assert = require('assert');
 var type = require('../');
 
-assert.equal(type.typeOf(undefined, 'undefined'), true, 'Error on value: undefined');
-assert.equal(type.typeOf(true, 'boolean'), true, 'Error on value: true');
-assert.equal(type.typeOf(false, 'boolean'), true, 'Error on value: false');
-assert.equal(type.typeOf(1, 'number'), true, 'Error on value: number 1');
-assert.equal(type.typeOf(1.2, 'number'), true, 'Error on value: number 1.2');
-assert.equal(type.typeOf('1', 'string'), true, 'Error on value: string "1"');
-assert.equal(type.typeOf(null, 'null'), true, 'Error on value: null');
-assert.equal(type.typeOf({}, 'object'), true, 'Error on value: {}');
-assert.equal(type.typeOf([], 'array'), true, 'Error on value: []');
-assert.equal(type.typeOf(new Date(), 'date'), true, 'Error on value: date');
-
-assert.equal(type.typeOf(new Date(), ['string', 'null', 'date']), true, 'Error on value: date');
+assert.equal(type.of(undefined, 'undefined'), true, 'Error on value: undefined');
+assert.equal(type.of(true, 'boolean'), true, 'Error on value: true');
+assert.equal(type.of(false, 'boolean'), true, 'Error on value: false');
+assert.equal(type.of(1, 'number'), true, 'Error on value: number 1');
+assert.equal(type.of(1.2, 'number'), true, 'Error on value: number 1.2');
+assert.equal(type.of('1', 'string'), true, 'Error on value: string "1"');
+assert.equal(type.of(null, 'null'), true, 'Error on value: null');
+assert.equal(type.of({}, 'object'), true, 'Error on value: {}');
+assert.equal(type.of([], 'array'), true, 'Error on value: []');
+assert.equal(type.of(new Date(), 'date'), true, 'Error on value: date');
+assert.equal(type.of(new Date(), ['string', 'null', 'date']), true, 'Error on value: date\'s');
+assert.equal(type.ofs([new Date(), new Date(), new Date()], ['string', 'null', 'date']), true, 'Error on value: date\'s from many');
