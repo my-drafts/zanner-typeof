b6b6a65f88ce0407c5286c60bbe9010d7ebf8071
Sergej Kukharev <sergej.kucharev@gmail.com>
2016-06-23 11:37:09 +0300

    1.0.6

--- test/index.js
+++ test/index.js
@@ -2,6 +2,34 @@
 var assert = require('assert');
 var type = require('../');
 
+console.log(type.is(undefined));
+console.log(type.is(true));
+console.log(type.is(false));
+console.log(type.is(123));
+console.log(type.is(new Number(123)));
+console.log(type.is('123'));
+console.log(type.is(new String('123')));
+console.log(type.is([1, 2, 3, 4]));
+console.log(type.is(new Array([1, 2, 3, 4])));
+console.log(type.is({a:1, b:2, c:3}));
+console.log(type.is({a:1, b:2, c:3}, true));
+console.log(type.is(new Object({a:1, b:2, c:3})));
+
+assert.equal(type.is(undefined), 'undefined', 'Error on is(undefined)');
+assert.equal(type.is(true), 'boolean', 'Error on is(true)');
+assert.equal(type.is(false), 'boolean', 'Error on is(false)');
+assert.equal(type.is(123), 'number', 'Error on is(123)');
+assert.equal(type.is(new Number(123)), 'number', 'Error on is(new Number(123))');
+assert.equal(type.is('123'), 'string', 'Error on is("123")');
+assert.equal(type.is(new String('123')), 'string', 'Error on is(new String("123"))');
+assert.equal(type.is([1, 2, 3, 4]), 'array', 'Error on is([1, 2, 3, 4])');
+assert.equal(type.is(new Array([1, 2, 3, 4])), 'array', 'Error on is(new Array([1, 2, 3, 4]))');
+assert.equal(type.is({a:1, b:2, c:3}), 'object', 'Error on is({a:1, b:2, c:3})');
+assert.equal(type.is({a:1, b:2, c:3}, 1), 'object', 'Error on is({a:1, b:2, c:3}, 1)');
+assert.equal(type.is({a:1, b:2, c:3}, -1), 'object', 'Error on is({a:1, b:2, c:3}, -1)');
+assert.equal(type.is(new Object({a:1, b:2, c:3})), 'object', 'Error on is(new Object({a:1, b:2, c:3}))');
+
+
 assert.equal(type.of(undefined, 'undefined'), true, 'Error on value: undefined');
 assert.equal(type.of(true, 'boolean'), true, 'Error on value: true');
 assert.equal(type.of(false, 'boolean'), true, 'Error on value: false');
--- typeof.js
+++ typeof.js
@@ -1,20 +1,14 @@
 
-var typeIs = module.exports.typeIs = function(that, objectFlag){
+var typeIs = module.exports.typeIs = function(that, thatFlag){
 	var like = {}.toString.call(that).toLowerCase().match(/^[\[]object[\s]+([a-zA-Z]+)[\]]$/)[1];
-	var name = objectFlag && like==='object' ? that.constructor.name.toLowerCase() : false;
-	if (name===false){
-		return like;
-	}
-	else if (objectFlag===false){
-		return like;
-	}
-	else if (objectFlag===true){
+	var name = thatFlag && like==='object' ? that.constructor.name.toLowerCase() : false;
+	if (name!==false && thatFlag===true){
 		return [name, like];
 	}
-	else if (objectFlag>0){
+	else if (name!==false && thatFlag>0){
 		return name;
 	}
-	else if (objectFlag<0){
+	else if (name===false || thatFlag===false || thatFlag<0){
 		return like;
 	}
 	else{
@@ -22,35 +16,41 @@ var typeIs = module.exports.typeIs = function(that, objectFlag){
 	}
 };
 
-var typeOf = module.exports.typeOf = function(that, name){
+var typeOf = module.exports.typeOf = function(that, name, thatFlag){
 	switch(typeIs(name)){
 		case 'array':
-			return name.some(function(name){
-				return typeOf(that, name);
+			return name.some(function(nameOne){
+				return typeOf(that, nameOne, thatFlag);
 			});
 			break;
+		case 'null':
+			return typeIs(that, thatFlag);
+			break;
+		case 'number':
+			return typeIs(that, name);
+			break;
 		case 'string':
-			return typeIs(that)===name;
+			return typeIs(that, typeIs(thatFlag)==='number' ? thatFlag : false)===name;
 			break;
 		case 'undefined':
-			return typeIs(that);
+			return typeIs(that, false);
 			break;
 		default:
-			throw 'Error in typeOf: 2 argument not type of [array, string, undefined]';
+			throw 'Error in typeOf: 2 argument not type of [array, null, number, string, undefined]';
 	};
 };
 
-var typeOfs = module.exports.typeOfs = function(thats, name){
-	if(!typeOf(name, ['array', 'string'])){
+var typeOfs = module.exports.typeOfs = function(thats, name, thatFlag){
+	if(!typeOf(name, ['array', 'string'], false)){
 		throw 'Error in typeOfs: 2 argument not type of [array, string]';
 	}
-	else if(typeOf(thats, 'array')){
-		return !thats.some(function(that){
-			return !typeOf(that, name);
-		});
+	else if(!typeOf(thats, 'array', false)){
+		throw 'Error in typeOfs: 1 argument not type of [array]';
 	}
 	else{
-		throw 'Error in typeOfs: 1 argument not type of [array]';
+		return !thats.some(function(that){
+			return !typeOf(that, name, thatFlag);
+		});
 	}
 };
 
